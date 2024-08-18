a544fe14c481e76c5ef98937601a29b31676695f
moolenbeek <emoolenbeek@gmail.com>
2024-08-18 13:35:50 -0400

    create simple weather app using skeleton ui

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,15 +1,50 @@
-<!-- YOU CAN DELETE EVERYTHING IN THIS PAGE -->
+<script lang="ts">
+	import { LightSwitch } from '@skeletonlabs/skeleton';
+	export let data;
+	console.log(data);
 
-<div class="container h-full mx-auto flex justify-center items-center">
-	<div class="space-y-5">
-		<h1 class="h1">Let's get cracking bones!</h1>
-		<p>Start by exploring:</p>
-		<ul>
-			<li><code class="code">/src/routes/+layout.svelte</code> - barebones layout</li>
-			<li><code class="code">/src/app.postcss</code> - app wide css</li>
-			<li>
-				<code class="code">/src/routes/+page.svelte</code> - this page, you can replace the contents
-			</li>
-		</ul>
-	</div>
+	let description = data.props.data.hourly[0].weather[0].main;
+	let icon = data.props.data.hourly[0].weather[0].icon;
+	let min = data.props.data.daily[0].temp.min;
+	let max = data.props.data.daily[0].temp.max;
+</script>
+
+<div class="p-4">
+	<LightSwitch />
+</div>
+
+<div class="card m-4 p-4 md:p-10 space-y-4 bg-white/10 dark:bg-black/10">
+	{#if data}
+
+	<header class="card-header">
+		<div class="flex items-center justify-center">
+			<p>Today</p>
+		</div>
+		<div class="flex items-center justify-center">
+			<h1>Ottawa, ON</h1>
+		</div>
+	</header>
+
+	<section class="p-4">
+		<div class="flex items-center justify-center">
+			<img src="https://openweathermap.org/img/wn/{icon}@2x.png" alt="" />
+		</div>
+		<div class="flex items-center justify-center">
+			<h1>{description}</h1>
+		</div>
+	</section>
+
+	<footer class="card-footer">
+		<div class="p-4 md:p-10 space-y-4 bg-white/10 dark:bg-black/10">
+			<div class="flex justify-center items-center mx-auto transition-[width] duration-200 w-full ">
+				<div class="w-full grid grid-cols-3 gap-4 place-items-center">
+					<p>&#8593 <b>{max} &#8451</b></p>
+					<span class="divider-vertical h-20" />
+					<p>&#8595 {min} &#8451</p>
+				</div>
+			</div>
+		</div>
+	</footer>
+
+	{/if}
 </div>
